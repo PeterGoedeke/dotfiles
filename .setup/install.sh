9a09a67ac7a562ae08dfbc997803b6b162f5e2b5
Peter Goedeke <peterbgoedeke@gmail.com>
2022-01-05 09:29:53 +1300

    fix: install script issues

--- .setup/install.sh
+++ .setup/install.sh
@@ -1,15 +1,14 @@
-sudo apt update
-sudo apt upgrade
+sudo apt update -y
+sudo apt upgrade -y
 
-sudo/apt-get install -y \
+sudo apt-get install -y \
 	git\
 	zsh\
 	python3\
 	autojump\
 	fzf
 
-echo ".cfg" >> .gitignore
-git clone --bare git@github.com:PeterGoedeke/dotfiles.git $HOME/.cfg
+git clone --bare https://github.com/PeterGoedeke/dotfiles $HOME/.cfg
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 config config --local status.showUntrackedFiles no
 config checkout
@@ -21,3 +20,4 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
