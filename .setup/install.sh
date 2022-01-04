df17a7858e78d91d47a212e1488179e90fc18b9d
Peter Goedeke <peterbgoedeke@gmail.com>
2022-01-05 10:31:36 +1300

    feat: support for yum

--- .setup/install.sh
+++ .setup/install.sh
@@ -1,15 +1,19 @@
-sudo apt update -y
-sudo apt upgrade -y
+if [[ sudo apt update -y && sudo apt upgrade -y ]]
+then
+    sudo apt-get install -y \
+        git\
+        zsh\
+        python3
+elif [[ sudo yum update ]]
+then
+    sudo yum -y install zsh
+fi
 
-sudo apt-get install -y \
-	git\
-	zsh\
-	python3\
-	autojump\
-	fzf
-
-chsh -s $(which zsh)
-# sudo chsh -s $(which zsh)
+mkdir .packages
+git clone git://github.com/wting/autojump.git $HOME/.packages/autojump
+$HOME/.packages/autojump/install.py
+git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.packages/fzf
+$HOME/.packages/fzf/install
 
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 
--- README.md
+++ README.md
@@ -3,5 +3,7 @@
 ```
 wget https://raw.githubusercontent.com/PeterGoedeke/dotfiles/master/.setup/install.sh &&
 chmod +x install.sh &&
-. ./install.sh
+. ./install.sh &&
+chsh -s $(which zsh) &&
+sudo chsh -s $(which zsh)
 ```
