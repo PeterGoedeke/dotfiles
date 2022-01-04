f99ca24cd8fe611a66a0652dc533fdefadac03c7
Peter Goedeke <peterbgoedeke@gmail.com>
2022-01-05 09:43:52 +1300

    fix: install fixes and README formatting
    - README block formatting
    - install script force checkout
    - install script starts zsh and sources .zshrc

--- .setup/install.sh
+++ .setup/install.sh
@@ -8,10 +8,13 @@ sudo apt-get install -y \
 	autojump\
 	fzf
 
+chsh -s $(which zsh)
+sudo chsh -s $(which zsh)
+
 git clone --bare https://github.com/PeterGoedeke/dotfiles $HOME/.cfg
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 config config --local status.showUntrackedFiles no
-config checkout
+config checkout -f
 
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 
@@ -21,3 +24,6 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+
+zsh
+source $HOME/.zshrc
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## Installation
 
-`wget https://raw.githubusercontent.com/PeterGoedeke/dotfiles/master/.setup/install.sh`
-
-`chmod +x install.sh`
-
-`./install.sh`
+```
+wget https://raw.githubusercontent.com/PeterGoedeke/dotfiles/master/.setup/install.sh &&
+chmod +x install.sh &&
+. ./install.sh
+```
