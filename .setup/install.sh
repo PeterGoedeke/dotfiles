eef0eaab8cf9d2f72badd20b875fa80d4d46e746
Peter Goedeke <peterbgoedeke@gmail.com>
2022-01-04 22:50:43 +1300

    feat: added install script and gitignore

---
+++ .setup/install.sh
@@ -0,0 +1,23 @@
+sudo apt update
+sudo apt upgrade
+
+sudo/apt-get install -y \
+	git\
+	zsh\
+	python3\
+	autojump\
+	fzf
+
+echo ".cfg" >> .gitignore
+git clone --bare git@github.com:PeterGoedeke/dotfiles.git $HOME/.cfg
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+config config --local status.showUntrackedFiles no
+config checkout
+
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
