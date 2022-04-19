566c1f15a5397a7ac1c4bda69fdc5197c5541218
Peter Goedeke <peterbgoedeke@gmail.com>
2022-04-19 22:49:14 +1200

    feat: update install script

--- .setup/install.sh
+++ .setup/install.sh
@@ -21,10 +21,7 @@ $HOME/.packages/fzf/install --all
 
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 
-git clone --bare https://github.com/PeterGoedeke/dotfiles $HOME/.cfg
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-config config --local status.showUntrackedFiles no
-config checkout -f
+git clone https://github.com/PeterGoedeke/dotfiles $HOME/dotfiles
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
@@ -33,5 +30,4 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 
-zsh
-source $HOME/.zshrc
+source "./link.sh"
