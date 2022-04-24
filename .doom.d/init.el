adf2f3854bbd9e8f6578918d47ce85a2f6b05ff9
Peter Goedeke <peterbgoedeke@gmail.com>
2022-04-24 15:14:26 +1200

    feat: added dictionary and thesaurus

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -93,7 +93,7 @@
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
-       lookup              ; navigate your code and its documentation
+       (lookup +dictionary +offline)              ; navigate your code and its documentation
        lsp               ; M-x vscode
        (magit +forge)             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
