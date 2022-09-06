3bedb4d720455719f1e61d9cddff20b5677b022f
Peter Goedeke <peterbgoedeke@gmail.com>
2022-09-06 22:04:56 +1200

    feat: fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,19 @@
+function vterm_printf;
+    if begin; [  -n "$TMUX" ]  ; and  string match -q -r "screen|tmux" "$TERM"; end
+        # tell tmux to pass the escape sequences through
+        printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
+    else if string match -q -- "screen*" "$TERM"
+        # GNU screen (screen, screen-256color, screen-256color-bce)
+        printf "\eP\e]%s\007\e\\" "$argv"
+    else
+        printf "\e]%s\e\\" "$argv"
+    end
+end
+
+set fish_greeting
+alias j="autojump"
+fzf_configure_bindings --variables
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
